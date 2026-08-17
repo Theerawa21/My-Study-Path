3ac0d875f3000b0a0243ba07d96061dd648b883d
Theerawa21 <numpanoof@gmail.com>
2026-08-17 09:24:06 +0700

    Connect frontend to Apps Script API

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// หลัง Deploy Google Apps Script เป็น Web app แล้ว ให้นำ URL ที่ลงท้ายด้วย /exec มาใส่ตรงนี้
+// Google Apps Script Web App API
 window.MY_STUDY_PATH_CONFIG = {
-  API_URL: '',
+  API_URL: 'https://script.google.com/macros/s/AKfycbw9XDJKPeXHHsGJ84tC8nFVkpLHx0pW0jv7JGNQzNGRdO9zBttA9mjnAMk5BE0r_xfDzQ/exec',
   APP_NAME: 'My Study Path',
   SCHOOL_NAME: ''
 };
