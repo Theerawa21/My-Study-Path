1b616bdf8b14078afc1873da21d0770c18be043e
Theerawa21 <numpanoof@gmail.com>
2026-08-17 09:16:42 +0700

    Add frontend configuration

---
+++ config.js
@@ -0,0 +1,6 @@
+// หลัง Deploy Google Apps Script เป็น Web app แล้ว ให้นำ URL ที่ลงท้ายด้วย /exec มาใส่ตรงนี้
+window.MY_STUDY_PATH_CONFIG = {
+  API_URL: '',
+  APP_NAME: 'My Study Path',
+  SCHOOL_NAME: ''
+};
